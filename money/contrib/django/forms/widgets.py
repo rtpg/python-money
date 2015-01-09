fe08987977b4ecc2dc894e01929b23aa7488118b
Paul Oswald <pauloswald@gmail.com>
2015-01-09 17:37:53 +0900

    PEP-8

--- money/contrib/django/forms/widgets.py
+++ money/contrib/django/forms/widgets.py
@@ -1,11 +1,11 @@
 from django import forms
-from money import Money, CURRENCY
-from decimal import Decimal
+
 
 class CurrencySelectWidget(forms.MultiWidget):
     """
     Custom widget for entering a value and choosing a currency
     """
+
     def __init__(self, choices=None, attrs=None):
         widgets = (
             forms.TextInput(attrs=attrs),
@@ -14,7 +14,6 @@ class CurrencySelectWidget(forms.MultiWidget):
         super(CurrencySelectWidget, self).__init__(widgets, attrs)
 
     def decompress(self, value):
-        #print "CurrencySelectWidget decompress %s" % value
         if value:
             return [value.amount, value.currency]
-        return [None,None]
+        return [None, None]
