391ef480cdccd2b04380539d92c95604089b8c17
Raphael Gaschignard <raphael@makeleaps.com>
2020-01-20 15:17:27 +0900

    Have the money field proxy descriptor behave properly
    Generally, descriptors tend to return themselves when accessed through the class (That is to say, when the instance is `None`)

--- money/contrib/django/models/fields.py
+++ money/contrib/django/models/fields.py
@@ -58,6 +58,8 @@ class MoneyFieldProxy(object):
         obj.__dict__[self.field.currency_field_name] = currency
 
     def __get__(self, obj, *args):
+        if obj is None:
+            return self
         amount, currency = self._get_values(obj)
         if amount is None:
             return None
